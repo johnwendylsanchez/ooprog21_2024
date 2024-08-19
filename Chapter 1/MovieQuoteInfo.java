12950734bde4a2314a9e0a668bc1834afb6422e5
johnwendylsanchez <johnwendylsanchez165@gmail.com>
2024-08-19 18:35:26 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+// MovieQuoteInfo.java
+// Chapter 1
+class MovieQuoteInfo
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Quote:This is the day you will always remember as the day you almost caught Captain Jack Sparrow.");
+      System.out.println("Movie:Jack Sparrow, Pirates of the Caribbean.");
+      System.out.println("Actor:Johnny Depp");
+      System.out.println("Year: 2003");
+   }
+}
