598fa828026a2fd17e313f113e3c3221bbdc10f8
johnwendylsanchez <johnwendylsanchez165@gmail.com>
2024-08-21 08:48:45 +0800

    TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -2,22 +2,18 @@
 // Chapter 1
 public class TableAndChairs {
     public static void main(String[] args) {
+        
         String chairLeg = "X                      X";
         String tableTop = "X      XXXXXXXXXX      X";
         String tableLegs = "XXXXX  X        X  XXXXX";
         String chairSeat = "X   X  X        X  X   X";
         
-        printLine(chairLeg);
-        printLine(chairLeg);
+        System.out.println(chairLeg);
+        System.out.println(chairLeg);
+        System.out.println(tableTop);
+        System.out.println(tableLegs);
         
-        printLine(tableTop);
-        
-        printLine(tableLegs);
-        printLine(chairSeat);
-        printLine(chairSeat);
-    }
-
-    private static void printLine(String line) {
-        System.out.println(line);
+        System.out.println(chairSeat);
+        System.out.println(chairSeat);
     }
 }
