408de21b2fb10b8979f24bdd422022401aefe709
JohnWendylSanchez <johnwendylsanchez165@gmail.com>
2024-09-02 21:17:34 +0800

    Add files via upload

---
+++ Chapter 2/WarmupSleepIn.java
@@ -0,0 +1,13 @@
+public class WarmupSleepIn {
+
+    public static void main(String[] args) {
+        
+        System.out.println(sleepIn(false, false)); 
+        System.out.println(sleepIn(true, false));  
+        System.out.println(sleepIn(false, true));  
+    }
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return weekday == false || vacation == true;
+    }
+}
