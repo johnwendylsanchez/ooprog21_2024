5179b1b5e35ed26e54e333e0b40420f3b71f3c0e
JohnWendylSanchez <johnwendylsanchez165@gmail.com>
2024-09-16 20:54:05 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,22 @@
+public class FirstAddressExercise {
+   
+   public static void main(String[] args) {
+   System.out.println("First Java Application");
+      displayAddress();
+      }
+      
+      public static void displayAddress() {
+      String add = "Oppo";
+      String district = "Dongcheng";
+      String capital = "Donnguan";
+      String zipCode = "510730";
+      String country = "China";
+      String companyName = "Oppo Mobile Telecommunications Corporation, Ltd.";
+      
+      System.out.println(add);
+      System.out.println(district + "," + capital + " " + zipCode);
+      System.out.println(country);
+      System.out.println("Company: " + companyName);
+      }
+    }
+      
